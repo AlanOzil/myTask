45c0636fcecffa60ef3b113b69cd760a8c26107a
AlanOzil <471855634@qq.com>
2017-03-22 15:41:13 +0800

    'aa'

--- js/fullcalendar/fullcalendar.js
+++ js/fullcalendar/fullcalendar.js
@@ -446,7 +446,7 @@ function Calendar(element, options, eventSources) {
 		}
 		else {
 			//suggestedViewHeight = Math.round(content.width() / Math.max(options.aspectRatio, .5));
-			suggestedViewHeight = $(window).height() - parseFloat($(".fc-header").css("height")) - 80;
+			suggestedViewHeight = $(window).height() - parseInt($(".fc-header").css("height")) - 81;
 		}
 	}
 	
